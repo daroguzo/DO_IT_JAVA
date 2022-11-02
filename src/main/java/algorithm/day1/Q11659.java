21ad2b92d779a736a07aeb7549339ff59c846ff6
daroguzo <kjw2122@naver.com>
2022-11-02 11:06:15 +0900

    221102 구간합 구하기

--- src/main/java/algorithm/day1/Q11659.java
+++ src/main/java/algorithm/day1/Q11659.java
@@ -1,9 +1,36 @@
 package algorithm.day1;
 
+import java.util.Scanner;
+
 /**
  * 구간 합
  * 구간 합 구하기
  * 실버3
+ * 2092ms
  */
 public class Q11659 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] array = new int[n + 1];
+        int[] answers = new int[m];
+
+        array[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int input = sc.nextInt();
+            array[i] = array[i - 1] + input;
+        }
+
+        for (int i = 0; i < m; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            answers[i] = array[y] - array[x - 1];
+        }
+
+        for (int i : answers) {
+            System.out.println(i);
+        }
+    }
 }
